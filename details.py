1b44cddda4a2b864559ac616e924617940bf3e57
divyeshsonu <44664822+divyeshsonu@users.noreply.github.com>
2024-11-09 07:07:55 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 26435700
-api_hash = "527cf5174e120a9093611bc69d7b7709"
-bot_token = "6180767554:AAGKw-Eo6L0DXlL4BsrDmKItUewgdKGAtGo"
+api_id = 28187462
+api_hash = "0159fbade6b803a808fbc5e248d52b87"
+bot_token = "7924605822:AAFc7To_ndjnk47DTPZKRu4apXTGYQiXwks"
 
